7e7aa5d80a27ef15525fbafd5023b4d8ee902720
Valraevn <131688256+Valraevn@users.noreply.github.com>
2023-05-18 22:16:57 +0100

    Update lists.py
    Improved

--- lists.py
+++ lists.py
@@ -1,25 +1,18 @@
 nakeds = ['vulcan', 'rebel', 'hornet', 'bandit']
-for naked in nakeds:
-        print(f"{naked.title()} is a bike")
-        print(f"{naked.title()} is a worthy bike.\n")
-print("Bikes go vroom")
-
 cruisers=['VN800', 'Intruder', 'SVX1100', 'vulcan']
-also_nakeds=nakeds[:]
-also_nakeds.append('bulldog')
 
 bikes=list(set(nakeds+cruisers))
-print(f"The full list of bikes is : \n{bikes}")
-print()
+print(f"\nThe full list of bikes is : \n{bikes}")
+
 print(f"of which these are nakeds:")
 for naked in nakeds:
         print(naked.title())
-print()
-print(f"and these are cruisers:")
+
+print(f"\nAnd these are cruisers:")
 for cruiser in cruisers:
         print(cruiser)
-print()
+
 perfect_bike=[bike for bike in cruisers if bike in nakeds]
-print("Hence the optimal bike; the bike in both lists is:")
+print("\nHence the optimal bike; the bike in both lists is:")
 for bike in perfect_bike:
         print(bike.title())
