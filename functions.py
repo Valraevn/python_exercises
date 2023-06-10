dededc968760a26931f3281676c474efc77afe1c
Valraevn <131688256+Valraevn@users.noreply.github.com>
2023-06-10 16:47:12 +0100

    Create functions.py
    A simple python example of functions functions.

---
+++ functions.py
@@ -0,0 +1,8 @@
+def make_shirt(size, text='Pepe'):
+    #display shirt info
+    print(f"\nI have a {size} tshirt with {text} on it.")
+
+make_shirt('xl', 'Role_model')
+make_shirt("M", "Urmum")
+#text call is default (Pepe)
+make_shirt(size='XL')
